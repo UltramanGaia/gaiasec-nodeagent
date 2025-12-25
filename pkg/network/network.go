a18f7e76ce9b54db92fc34c5dd307719f885649c
UltramanGaia <3213359017@qq.com>
2025-12-25 20:11:02 +0800

    网络信息

--- pkg/network/network.go
+++ pkg/network/network.go
@@ -36,6 +36,34 @@ func GetNetworkConnections() ([]*pb.NetworkConnection, error) {
 		connections = append(connections, conn)
 	}
 
+	// Get TCP6 connections
+	tcp6Socks, err := TCP6Socks(NoopFilter)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, sock := range tcp6Socks {
+		pid := int32(0)
+		processName := ""
+
+		if sock.Process != nil {
+			pid = int32(sock.Process.Pid)
+			processName = sock.Process.Name
+		}
+
+		conn := &pb.NetworkConnection{
+			Protocol:      "tcp6",
+			LocalAddress:  sock.LocalAddr.IP.String(),
+			LocalPort:     int32(sock.LocalAddr.Port),
+			RemoteAddress: sock.RemoteAddr.IP.String(),
+			RemotePort:    int32(sock.RemoteAddr.Port),
+			Pid:           pid,
+			Uid:           0, // go-netstat库不提供UID信息
+			ProcessName:   processName,
+		}
+		connections = append(connections, conn)
+	}
+
 	// Get UDP connections
 	udpSocks, err := UDPSocks(NoopFilter)
 	if err != nil {
@@ -53,6 +81,34 @@ func GetNetworkConnections() ([]*pb.NetworkConnection, error) {
 
 		conn := &pb.NetworkConnection{
 			Protocol:      "udp",
+			LocalAddress:  sock.LocalAddr.IP.String(),
+			LocalPort:     int32(sock.LocalAddr.Port),
+			RemoteAddress: sock.RemoteAddr.IP.String(),
+			RemotePort:    int32(sock.RemoteAddr.Port),
+			Pid:           pid,
+			Uid:           0, // go-netstat库不提供UID信息
+			ProcessName:   processName,
+		}
+		connections = append(connections, conn)
+	}
+
+	// Get UDP6 connections
+	udp6Socks, err := UDP6Socks(NoopFilter)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, sock := range udp6Socks {
+		pid := int32(0)
+		processName := ""
+
+		if sock.Process != nil {
+			pid = int32(sock.Process.Pid)
+			processName = sock.Process.Name
+		}
+
+		conn := &pb.NetworkConnection{
+			Protocol:      "udp6",
 			LocalAddress:  sock.LocalAddr.IP.String(),
 			LocalPort:     int32(sock.LocalAddr.Port),
 			RemoteAddress: sock.RemoteAddr.IP.String(),
