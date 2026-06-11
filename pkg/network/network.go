124bcac921abc38d998393cb7e1a7f6f5dc7ef03
UltramanGaia <3213359017@qq.com>
2026-06-11 06:37:26 +0000

    refactor(network): tolerate partial socket lookup failures in GetNetworkConnections
    Previously, any error from TCPSocks/TCP6Socks/UDPSocks/UDP6Socks would
    cause GetNetworkConnections to return immediately with no results. Now
    each protocol is collected independently; failures are silently skipped
    so that available connections are still returned.

--- pkg/network/network.go
+++ pkg/network/network.go
@@ -10,92 +10,106 @@ func GetNetworkConnections() ([]*pb.NetworkConnection, error) {
 
 	// Get TCP connections
 	tcpSocks, err := TCPSocks(NoopFilter)
-	if err != nil {
-		return nil, err
-	}
+	if err == nil {
+		for _, sock := range tcpSocks {
+			pid := int32(0)
+			processName := ""
 
-	for _, sock := range tcpSocks {
-		pid := int32(0)
-		processName := ""
+			if sock.Process != nil {
+				pid = int32(sock.Process.Pid)
+				processName = sock.Process.Name
+			}
 
-		if sock.Process != nil {
-			pid = int32(sock.Process.Pid)
-			processName = sock.Process.Name
+			conn := &pb.NetworkConnection{
+				Protocol:      "tcp",
+				LocalAddress:  sock.LocalAddr.IP.String(),
+				LocalPort:     int32(sock.LocalAddr.Port),
+				RemoteAddress: sock.RemoteAddr.IP.String(),
+				RemotePort:    int32(sock.RemoteAddr.Port),
+				Pid:           pid,
+				Uid:           0, // go-netstat库不提供UID信息
+				ProcessName:   processName,
+			}
+			connections = append(connections, conn)
 		}
-
-		conn := &pb.NetworkConnection{
-			Protocol:      "tcp",
-			LocalAddress:  sock.LocalAddr.IP.String(),
-			LocalPort:     int32(sock.LocalAddr.Port),
-			RemoteAddress: sock.RemoteAddr.IP.String(),
-			RemotePort:    int32(sock.RemoteAddr.Port),
-			Pid:           pid,
-			Uid:           0, // go-netstat库不提供UID信息
-			ProcessName:   processName,
-		}
-		connections = append(connections, conn)
 	}
 
 	// Get TCP6 connections
 	tcp6Socks, err := TCP6Socks(NoopFilter)
-	if err != nil {
-		return nil, err
-	}
+	if err == nil {
+		for _, sock := range tcp6Socks {
+			pid := int32(0)
+			processName := ""
 
-	for _, sock := range tcp6Socks {
-		pid := int32(0)
-		processName := ""
+			if sock.Process != nil {
+				pid = int32(sock.Process.Pid)
+				processName = sock.Process.Name
+			}
 
-		if sock.Process != nil {
-			pid = int32(sock.Process.Pid)
-			processName = sock.Process.Name
+			conn := &pb.NetworkConnection{
+				Protocol:      "tcp6",
+				LocalAddress:  sock.LocalAddr.IP.String(),
+				LocalPort:     int32(sock.LocalAddr.Port),
+				RemoteAddress: sock.RemoteAddr.IP.String(),
+				RemotePort:    int32(sock.RemoteAddr.Port),
+				Pid:           pid,
+				Uid:           0, // go-netstat库不提供UID信息
+				ProcessName:   processName,
+			}
+			connections = append(connections, conn)
 		}
-
-		conn := &pb.NetworkConnection{
-			Protocol:      "tcp6",
-			LocalAddress:  sock.LocalAddr.IP.String(),
-			LocalPort:     int32(sock.LocalAddr.Port),
-			RemoteAddress: sock.RemoteAddr.IP.String(),
-			RemotePort:    int32(sock.RemoteAddr.Port),
-			Pid:           pid,
-			Uid:           0, // go-netstat库不提供UID信息
-			ProcessName:   processName,
-		}
-		connections = append(connections, conn)
 	}
 
 	// Get UDP connections
 	udpSocks, err := UDPSocks(NoopFilter)
-	if err != nil {
-		return nil, err
-	}
+	if err == nil {
+		for _, sock := range udpSocks {
+			pid := int32(0)
+			processName := ""
 
-	for _, sock := range udpSocks {
-		pid := int32(0)
-		processName := ""
+			if sock.Process != nil {
+				pid = int32(sock.Process.Pid)
+				processName = sock.Process.Name
+			}
 
-		if sock.Process != nil {
-			pid = int32(sock.Process.Pid)
-			processName = sock.Process.Name
+			conn := &pb.NetworkConnection{
+				Protocol:      "udp",
+				LocalAddress:  sock.LocalAddr.IP.String(),
+				LocalPort:     int32(sock.LocalAddr.Port),
+				RemoteAddress: sock.RemoteAddr.IP.String(),
+				RemotePort:    int32(sock.RemoteAddr.Port),
+				Pid:           pid,
+				Uid:           0, // go-netstat库不提供UID信息
+				ProcessName:   processName,
+			}
+			connections = append(connections, conn)
 		}
-
-		conn := &pb.NetworkConnection{
-			Protocol:      "udp",
-			LocalAddress:  sock.LocalAddr.IP.String(),
-			LocalPort:     int32(sock.LocalAddr.Port),
-			RemoteAddress: sock.RemoteAddr.IP.String(),
-			RemotePort:    int32(sock.RemoteAddr.Port),
-			Pid:           pid,
-			Uid:           0, // go-netstat库不提供UID信息
-			ProcessName:   processName,
-		}
-		connections = append(connections, conn)
 	}
 
 	// Get UDP6 connections
 	udp6Socks, err := UDP6Socks(NoopFilter)
-	if err != nil {
-		return nil, err
+	if err == nil {
+		for _, sock := range udp6Socks {
+			pid := int32(0)
+			processName := ""
+
+			if sock.Process != nil {
+				pid = int32(sock.Process.Pid)
+				processName = sock.Process.Name
+			}
+
+			conn := &pb.NetworkConnection{
+				Protocol:      "udp6",
+				LocalAddress:  sock.LocalAddr.IP.String(),
+				LocalPort:     int32(sock.LocalAddr.Port),
+				RemoteAddress: sock.RemoteAddr.IP.String(),
+				RemotePort:    int32(sock.RemoteAddr.Port),
+				Pid:           pid,
+				Uid:           0, // go-netstat库不提供UID信息
+				ProcessName:   processName,
+			}
+			connections = append(connections, conn)
+		}
 	}
 
 	for _, sock := range udp6Socks {
